4666688a0f453df7fa8a4ba1b0cf0e9beaa46590
HisdaelePro <hisdaele.kamga@gmail.com>
2021-04-15 13:57:06 +0000

    first commit

---
+++ workspace/images/splitData.py
@@ -0,0 +1,15 @@
+import os.path
+import sys
+import shutil
+
+imgFolderPath = sys.argv[1]
+
+numberOfFiles = 1
+for file in os.listdir(imgFolderPath):
+	if file.endswith("jpg") or file.endswith("jpeg") or file.endswith("png"):
+		if numberOfFiles <= 6124:
+			shutil.move(file, "train/" + file)
+		else:
+			shutil.move(file, "test/" + file)
+			
+		numberOfFiles = numberOfFiles + 1
