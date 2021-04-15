0290c109439e0e40bdafc0428141da79eb69151e
HisdaelePro <hisdaele.kamga@gmail.com>
2021-04-15 14:29:26 +0000

    make some modifications

--- workspace/images/splitData.py
+++ workspace/images/splitData.py
@@ -7,9 +7,9 @@ imgFolderPath = sys.argv[1]
 numberOfFiles = 1
 for file in os.listdir(imgFolderPath):
 	if file.endswith("jpg") or file.endswith("jpeg") or file.endswith("png"):
-		if numberOfFiles <= 6124:
+		if numberOfFiles <= 1000:
 			shutil.move(file, "train/" + file)
 		else:
 			shutil.move(file, "test/" + file)
-			
+
 		numberOfFiles = numberOfFiles + 1
